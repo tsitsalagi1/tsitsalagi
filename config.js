1f0bed93c9e509963ca1072f67d2ffee843496e4
tsitsalagi1 <dynamictech.nwa@gmail.com>
2026-06-14 01:10:09 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.TSITSALAGI_CONFIG = {
 
   issuesCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEhBppk6ms1fnkgsTYObjkZQgDvEPPZORvZyLOJS0I-mPy2UOEQXxE7jn_yvulB-vvd-iGK7mSDZNz/pub?gid=1338591729&single=true&output=csv",
 
-  resourcesCsvUrl: "data/resources.csv",
+  resourcesCsvUrl: "resources.csv",
 
   listingFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSeOazBvPNLRTc0NumD5oV3TnajeoD4rdMq0UO--4HQTtMmcaA/viewform?usp=sharing&ouid=101078332389551426862",
 
