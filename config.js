418cca3a6d1694bcb4d0e3d93b533525d5dfd498
tsitsalagi1 <dynamictech.nwa@gmail.com>
2026-06-13 21:28:17 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 window.TSITSALAGI_CONFIG = {
   listingsCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEhBppk6ms1fnkgsTYObjkZQgDvEPPZORvZyLOJS0I-mPy2UOEQXxE7jn_yvulB-vvd-iGK7mSDZNz/pub?gid=0&single=true&output=csv",
-
   issuesCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEhBppk6ms1fnkgsTYObjkZQgDvEPPZORvZyLOJS0I-mPy2UOEQXxE7jn_yvulB-vvd-iGK7mSDZNz/pub?gid=1338591729&single=true&output=csv",
-
   resourcesCsvUrl: "data/resources.csv",
-
-  listingFormUrl: "#",
-  issueFormUrl: "#",
+  listingFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSeOazBvPNLRTc0NumD5oV3TnajeoD4rdMq0UO--4HQTtMmcaA/viewform?usp=sharing&ouid=101078332389551426862",
+  issueFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLScwWnvZpnYbh8mB_4wduYEjcbMXQnIX2jWunjZQEOENxBJ8iA/viewform?usp=sharing&ouid=101078332389551426862",
   contactEmail: "dynamictech.nwa@gmail.com"
 };
