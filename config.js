d982baad7e68347e50b465e888ff11150da9118e
tsitsalagi1 <dynamictech.nwa@gmail.com>
2026-06-13 23:52:03 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
+// Tsitsalagi configuration
+// This file powers public approved listings/issues from published Google Sheets CSV links.
+// Form URLs can be added later when you copy the public responder links from Google Forms.
+
 window.TSITSALAGI_CONFIG = {
   listingsCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEhBppk6ms1fnkgsTYObjkZQgDvEPPZORvZyLOJS0I-mPy2UOEQXxE7jn_yvulB-vvd-iGK7mSDZNz/pub?gid=0&single=true&output=csv",
   issuesCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEhBppk6ms1fnkgsTYObjkZQgDvEPPZORvZyLOJS0I-mPy2UOEQXxE7jn_yvulB-vvd-iGK7mSDZNz/pub?gid=1338591729&single=true&output=csv",
   resourcesCsvUrl: "data/resources.csv",
-  listingFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSeOazBvPNLRTc0NumD5oV3TnajeoD4rdMq0UO--4HQTtMmcaA/viewform?usp=sharing&ouid=101078332389551426862",
-  issueFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLScwWnvZpnYbh8mB_4wduYEjcbMXQnIX2jWunjZQEOENxBJ8iA/viewform?usp=sharing&ouid=101078332389551426862",
-  contactEmail: "tsitsalagi.com@gmail.com"
+
+  // Paste public Google Form responder links here later.
+  listingFormUrl: "",
+  issueFormUrl: "",
+
+  contactEmail: "dynamictech.nwa@gmail.com",
+  showUnapproved: false
 };
