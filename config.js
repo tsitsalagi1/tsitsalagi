dcb0de9950fa5294b6d5e9b1382578982a47c973
tsitsalagi1 <dynamictech.nwa@gmail.com>
2026-06-13 20:10:34 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
-const TSITSALAGI_CONFIG = {
+window.TSITSALAGI_CONFIG = {
   listingsCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEhBppk6ms1fnkgsTYObjkZQgDvEPPZORvZyLOJS0I-mPy2UOEQXxE7jn_yvulB-vvd-iGK7mSDZNz/pub?gid=0&single=true&output=csv",
+
   issuesCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEhBppk6ms1fnkgsTYObjkZQgDvEPPZORvZyLOJS0I-mPy2UOEQXxE7jn_yvulB-vvd-iGK7mSDZNz/pub?gid=1338591729&single=true&output=csv",
-  resourcesCsvUrl: "data/resources.csv"
+
+  resourcesCsvUrl: "data/resources.csv",
+
+  listingFormUrl: "#",
+  issueFormUrl: "#",
+  contactEmail: "dynamictech.nwa@gmail.com"
 };
